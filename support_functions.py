76cef6b53b4a467c5cbd2cb58b265d92fbf69086
vladimirjankov <vlajkojjj@gmail.com>
2019-03-28 00:25:08 +0100

    added precentage_normalise, percentage_binary_normalise, normalise_data fixed

--- support_functions.py
+++ support_functions.py
@@ -22,15 +22,51 @@ def distance_matrix_np(data,metr):
 
 def normalise_data(data):
         data_normed = data
-        colum_sum = np.sum(data,axis=0)
+        
         i = 0
         j = 0 
         for i in range(0,data.shape[0]):
-                for j in range(0,data.shape[1]):
-                        data_normed[i,j]  = data[i,j]/colum_sum[j]
+                colum_sum = np.sum(data[i,:])
+                for j in range(0,data.shape[1]):                       
+                        data_normed[i,j]  = data[i,j]/colum_sum
                 
+        return data_normed
 
-        return data_normed        
+def percentage_normalise(data,p1,p2):
+        # p1 treshold ispod kog je gen ne bitan
+        # p2 treshold zastupljenosti gena u svim uzorcima 
+
+        data = normalise_data(data)
+        data_smaller = np.array([])
+        counter = 0
+        for i in range(0,data.shape[1]):
+                data[:,i] = data[:,i]/max(data[:,i])
+                for j in range(0, data.shape[0]):
+                        if data[j,i] < p1:
+                                data[j,i] = 0
+                                counter = counter + 1
+                
+                if (counter/float(data.shape[0])) >p2 : 
+                        data_smaller.append(data[:,i])
+        return data_smaller
+
+def percentage_binary_normalise(data,p1,p2):
+        # p1 treshold ispod kog je gen ne bitan
+        # p2 treshold zastupljenosti gena u svim uzorcima 
+
+        data = normalise_data(data)
+        data_smaller = np.array([])
+        counter = 0
+        for i in range(0,data.shape[1]):
+                data[:,i] = data[:,i]/max(data[:,i])
+                data[:,i] = data[:,i] <p1
+                counter = np.sum(data[:,i])
+                if (counter/float(data.shape[0])) >p2 : 
+                        data_smaller.append(data[:,i])
+                        
+        return data_smaller
+                        
+
 
 
 
