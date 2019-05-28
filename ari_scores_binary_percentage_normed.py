576d21f36a732d9001ea9da6bbadc48be3be05f8
vladimirjankov <vlajkojjj@gmail.com>
2019-05-28 14:58:56 +0200

    nwew commit

--- ari_scores_binary_percentage_normed.py
+++ ari_scores_binary_percentage_normed.py
@@ -15,36 +15,44 @@ def ari_scores_binary_percentage_normed(data,labels,file,p1,p2):
     data = np.transpose(percentage_binary_normalise(data,p1,p2) )
     labels = np.array(labels)
     lbls =np.reshape(labels,(labels.shape[0],))
-
-    dice_label = k_means(data, np.max(np.unique(labels)), dice)
-    ari_dice = metrics.adjusted_rand_score(lbls,dice_label)
+    ari_dice =0
+    ari_hamming =0
+    ari_jaccard = 0
+    ari_rogerstanimoto =0
+    ari_russellrao=0
+    ari_sokalmichener=0
+    ari_sokalsneath=0
+    ari_yule= 0
+    for i in range(0,25):
+        dice_label = k_means(data, np.max(np.unique(labels)), dice)
+        ari_dice = ari_dice+ metrics.adjusted_rand_score(lbls,dice_label)
  #radi normalno
 
-    hamming_label = k_means(data, np.max(np.unique(labels)), hamming)
-    ari_hamming = metrics.adjusted_rand_score(lbls,hamming_label)
+        hamming_label = k_means(data, np.max(np.unique(labels)), hamming)
+        ari_hamming =ari_hamming+ metrics.adjusted_rand_score(lbls,hamming_label)
 #radi normalno
 
-    jaccard_label = k_means(data, np.max(np.unique(labels)), jaccard)
-    ari_jaccard = metrics.adjusted_rand_score(lbls,jaccard_label)
+        jaccard_label = k_means(data, np.max(np.unique(labels)), jaccard)
+        ari_jaccard =ari_jaccard+ metrics.adjusted_rand_score(lbls,jaccard_label)
 #radi normalno
-    rogerstanimoto_label = k_means(data, np.max(np.unique(labels)), rogerstanimoto)
-    ari_rogerstanimoto = metrics.adjusted_rand_score(lbls,rogerstanimoto_label)
+        rogerstanimoto_label = k_means(data, np.max(np.unique(labels)), rogerstanimoto)
+        ari_rogerstanimoto = ari_rogerstanimoto+metrics.adjusted_rand_score(lbls,rogerstanimoto_label)
 #radi normalno
-    russellrao_label = k_means(data, np.max(np.unique(labels)), russellrao)
-    ari_russellrao = metrics.adjusted_rand_score(lbls,russellrao_label)
-#radi normalno
-
-    sokalmichener_label = k_means(data, np.max(np.unique(labels)), sokalmichener)
-    ari_sokalmichener = metrics.adjusted_rand_score(lbls,sokalmichener_label)
+        russellrao_label = k_means(data, np.max(np.unique(labels)), russellrao)
+        ari_russellrao =ari_russellrao+ari_russellrao+ metrics.adjusted_rand_score(lbls,russellrao_label)
 #radi normalno
 
-    sokalsneath_label = k_means(data, np.max(np.unique(labels)), sokalsneath)
-    ari_sokalsneath = metrics.adjusted_rand_score(lbls,sokalsneath_label)
-#radi normalno
-    yule_label = k_means(data, np.max(np.unique(labels)), yule)
-    ari_yule = metrics.adjusted_rand_score(lbls,yule_label)
+        sokalmichener_label = k_means(data, np.max(np.unique(labels)), sokalmichener)
+        ari_sokalmichener =ari_sokalmichener+ metrics.adjusted_rand_score(lbls,sokalmichener_label)
 #radi normalno
 
+        sokalsneath_label = k_means(data, np.max(np.unique(labels)), sokalsneath)
+        ari_sokalsneath = ari_sokalsneath + metrics.adjusted_rand_score(lbls,sokalsneath_label)
+#radi normalno
+        yule_label = k_means(data, np.max(np.unique(labels)), yule)
+        ari_yule = ari_yule +metrics.adjusted_rand_score(lbls,yule_label)
+#radi normalno
+    tmp = np.array([ari_dice,ari_hamming,ari_jaccard,ari_rogerstanimoto,ari_russellrao,ari_russellrao,ari_sokalmichener,ari_sokalsneath,ari_yule])/25
 
-    return [file,ari_dice,ari_hamming,ari_jaccard,ari_rogerstanimoto,ari_russellrao,ari_russellrao,ari_sokalmichener,ari_sokalsneath,ari_yule]
+    return [file,tmp.tolist()]
 
--- ari_scores_zpercentage.py
+++ ari_scores_zpercentage.py
@@ -17,39 +17,51 @@ def ari_scores_zpercentage_normed(data,labels,name,p1,p2):
     data = np.transpose(percentage_normalise_z(data,p1,p2) )
     labels = np.array(labels)
     lbls =np.reshape(labels,(labels.shape[0],))
-    cosine_label = k_means(data, np.max(np.unique(labels)),cosine_distance)
-    ari_cosine = metrics.adjusted_rand_score(lbls,cosine_label)
 
-    braycurtis_label = k_means(data, np.max(np.unique(labels)), braycurtis)
-    ari_braycurtis = metrics.adjusted_rand_score(lbls,braycurtis_label)
+    ari_cosine =0 
+    ari_braycurtis =0
+    ari_correlation=0
+    ari_canberra=0
+    ari_hellinger=0
+    ari_wasserstein=0
+    ari_energy=0
+    ari_kulczynski=0
+    ari_eucl=0
+    for i in range(0,25):
+        cosine_label = k_means(data, np.max(np.unique(labels)),cosine_distance)
+        ari_cosine = ari_cosine+metrics.adjusted_rand_score(lbls,cosine_label)
+
+        braycurtis_label = k_means(data, np.max(np.unique(labels)), braycurtis)
+        ari_braycurtis = ari_braycurtis+metrics.adjusted_rand_score(lbls,braycurtis_label)
  #radi normalno
 
-    correlation_label = k_means(data, np.max(np.unique(labels)), correlation)
-    ari_correlation = metrics.adjusted_rand_score(lbls,correlation_label)
+        correlation_label = k_means(data, np.max(np.unique(labels)), correlation)
+        ari_correlation =ari_correlation+ metrics.adjusted_rand_score(lbls,correlation_label)
 #radi normalno
 
-    canberra_label = k_means(data, np.max(np.unique(labels)), canberra)
-    ari_canberra = metrics.adjusted_rand_score(lbls,canberra_label)
+        canberra_label = k_means(data, np.max(np.unique(labels)), canberra)
+        ari_canberra = ari_canberra+ metrics.adjusted_rand_score(lbls,canberra_label)
 #radi normalno
-  #  hellinger_label = k_means(data, np.max(np.unique(labels)), hellinger)
- #   ari_hellinger = metrics.adjusted_rand_score(lbls,hellinger_label)
+    #    hellinger_label = k_means(data, np.max(np.unique(labels)), hellinger)
+    #    ari_hellinger =ari_hellinger+ metrics.adjusted_rand_score(lbls,hellinger_label)
 #radi normalno
-    wasserstein_distance_label = k_means(data, np.max(np.unique(labels)), wasserstein_distance)
-    ari_wasserstein = metrics.adjusted_rand_score(lbls,wasserstein_distance_label)
-#radi normalno
-
-    energy_distance_label = k_means(data, np.max(np.unique(labels)), energy_distance)
-    ari_energy = metrics.adjusted_rand_score(lbls,energy_distance_label)
+        wasserstein_distance_label = k_means(data, np.max(np.unique(labels)), wasserstein_distance)
+        ari_wasserstein = ari_wasserstein+ metrics.adjusted_rand_score(lbls,wasserstein_distance_label)
 #radi normalno
 
-    kulczynski_label = k_means(data, np.max(np.unique(labels)), dist_kulczynski_vectors)
-    ari_kulczynski = metrics.adjusted_rand_score(lbls,kulczynski_label)
-#radi normalno
-    eucl_label = k_means(data, np.max(np.unique(labels)), euclidean)
-    ari_eucl = metrics.adjusted_rand_score(lbls,eucl_label)
+        energy_distance_label = k_means(data, np.max(np.unique(labels)), energy_distance)
+        ari_energy = ari_energy + metrics.adjusted_rand_score(lbls,energy_distance_label)
 #radi normalno
 
+        kulczynski_label = k_means(data, np.max(np.unique(labels)), dist_kulczynski_vectors)
+        ari_kulczynski = ari_kulczynski+metrics.adjusted_rand_score(lbls,kulczynski_label)
+#radi normalno
+        eucl_label = k_means(data, np.max(np.unique(labels)), euclidean)
+        ari_eucl = ari_eucl + metrics.adjusted_rand_score(lbls,eucl_label)
+#radi normalno
+    tmp = np.array([ari_cosine,ari_braycurtis,ari_correlation,ari_canberra,ari_wasserstein,ari_energy,ari_kulczynski,ari_eucl]) / 25
 
-    return [name,ari_cosine,ari_braycurtis,ari_correlation,ari_canberra,ari_wasserstein,ari_energy,ari_kulczynski,ari_eucl]
+    return [name, tmp.tolist()]
+
 # -*- coding: utf-8 -*-
 #ari_hellinger
